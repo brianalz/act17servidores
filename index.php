fcfd5d4c85d816ef7023931c82a2a7e334947e9c
Brian Alz <>
2023-03-29 13:56:50 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+  <head>
+    <title>Datos De Informacion</title>
+  </head>
+  <body>
+    <h2>Datos De Informacion</h2>
+    <?php 
+     echo 'Nombre:'.'Brian Anthoan Almanza Martinez'
+     ?> 
+    <br>
+    <?php 
+     echo 'Calle:'.'Blvd Eulalio Gutierrez'
+     ?>
+    <br>
+    <?php 
+    echo 'Numero de Casa:'.'#8888'
+    ?> 
+    <br>
+    <?php 
+    echo 'Municipio:'.'Saltillo'
+    ?> 
+    <br>
+    <?php 
+    echo 'Colonia:'.'Valle De Las Flores'
+    ?> 
+    <br>
+        <?php 
+    echo 'Telefono:'.'8441234567'
+    ?> 
+    <br>
+   
+  </body>
+</html>
---
+++ read.md
@@ -0,0 +1,14 @@
+1. Inicio
+2. leer Nombre
+3. Mostrar "Brian Anthoan Almanza Martinez"
+4. leer Calle
+5. Mostrar "Blvd Eulalio Gutierrez"
+6. leer Numero de Casa
+7. Mostrar "#8888"
+8. leer Municipio
+9. Mostar "Saltillo"
+10. leer Colonia
+11. Mostrar "Valle De Las Flores"
+12. leer Telefono
+13. Mostrar "8441234567"
+14. Fin
